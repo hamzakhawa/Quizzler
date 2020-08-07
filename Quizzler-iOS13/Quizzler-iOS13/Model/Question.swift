fe913c994ea89c14b33df1bb6f44404d37ef2b2c
hamzakhawa <64156452+hamzakhawa@users.noreply.github.com>
2020-08-07 23:14:31 +0400

    Add files via upload

---
+++ Quizzler-iOS13/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Quizzler-iOS13
+//
+//  Created by Hamza Khawaja on 7/25/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
